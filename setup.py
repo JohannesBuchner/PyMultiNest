a90906c49ca1e7ad09633b017b5115b20be655d2
Johannes Buchner <johannes.buchner.acad@gmx.com>
2012-01-05 19:35:33 +0100

    added some more info to package

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 try:
     from setuptools import setup
 except:
@@ -6,5 +9,10 @@ except:
 setup(
     name = "pymultinest",
     version = "0.1",
+    description = "Access modules for MultiNest and APEMoST",
+    author = "Johannes Buchner",
+    author_email = "johannes.buchner.acad [ät] gmx.com",
+    url = "http://johannesbuchner.github.com/PyMultiNest/",
+    license = "GPLv3",
     packages = ["pymultinest", "pyapemost"]
 )
