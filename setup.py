c6ca8999c44e06b426306b49101e9bd0c36701c5
Johannes Buchner <johannes.buchner.acad@gmx.com>
2012-05-07 19:34:26 +0200

    some more dep info

--- setup.py
+++ setup.py
@@ -11,8 +11,13 @@ setup(
     version = "0.1",
     description = "Access modules for MultiNest and APEMoST",
     author = "Johannes Buchner",
-    author_email = "johannes.buchner.acad [ät] gmx.com",
+    author_email = "johannes.buchner.acad [@t] gmx.com",
+    maintainer = "Johannes Buchner",
+    maintainer_email = "johannes.buchner.acad [@t] gmx.com",
     url = "http://johannesbuchner.github.com/PyMultiNest/",
     license = "GPLv3",
-    packages = ["pymultinest", "pyapemost", "pycuba"]
+    packages = ["pymultinest", "pyapemost", "pycuba"],
+    provides = ["pymultinest", "pyapemost", "pycuba"],
+    requires = ["ctypesGsl", "numpy (>=1.5)", "matplotlib", "scipy"]
 )
+
