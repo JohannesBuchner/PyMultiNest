a5c070b1db27c435ad597807305d63efdc19d7b9
Matt Pitkin <m@ttpitk.in>
2020-08-05 14:49:16 +0100

    Update pymultinest_long_name_test.py
     - remove tabs

--- tests/pymultinest_long_name_test.py
+++ tests/pymultinest_long_name_test.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     from pymultinest.solve import solve
 
     solve(lambda cube: -0.5 *(cube**2).sum(), lambda cube: cube, 2, 
-	      resume=True, verbose=True,
+          resume=True, verbose=True,
           outputfiles_basename="a_really_really_really_really_really_really_really_really_really_really_really_really_really_really_really_really_really_long_name-")
