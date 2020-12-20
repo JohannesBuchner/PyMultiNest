7df12385d76dbe3aea2b4f1ccc128b4de4413578
Johannes Buchner <johannes.buchner.acad@gmx.com>
2020-12-20 18:17:14 +0100

    version bump to 2.11

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as file:
 
 setup(
     name = "pymultinest",
-    version = "2.10",
+    version = "2.11",
     description = "Access modules for the MultiNest and Cuba integration libraries",
     author = "Johannes Buchner",
     author_email = "johannes.buchner.acad@gmx.com",
